fe09c458e880c227ce8effe929854c7007f634c5
Konrad Kreczko <konradkreczko@gmail.com>
2024-12-04 19:43:49 +0100

    feat: Created user model

--- src/main/java/com/example/movieservice/model/User.java
+++ src/main/java/com/example/movieservice/model/User.java
@@ -1,4 +1,41 @@
 package com.example.movieservice.model;
 
+
+import com.example.movieservice.model.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Column(unique = true)
+    @NotBlank
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToMany
+    private Set<Movie> favoriteMovies;
+
+    @ManyToMany
+    private Set<Movie> watchList;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Review> reviews;
 }
