9aebd660c6677c73a1855d131a69ad8028726c87
Konrad Kreczko <konradkreczko@gmail.com>
2024-12-04 19:54:00 +0100

    feat: Created review model

--- src/main/java/com/example/movieservice/model/Review.java
+++ src/main/java/com/example/movieservice/model/Review.java
@@ -1,4 +1,29 @@
 package com.example.movieservice.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int rating;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Movie movie;
+
+    @OneToMany(mappedBy = "review")
+    private Set<Comment> comments;
+
+    private LocalDate createdDate;
 }
